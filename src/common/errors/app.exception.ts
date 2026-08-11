c0ba02e132244d97e7a7f72cdbe70f46818be2f2
camisplai <camjarag12gmail.com>
2026-08-11 14:32:10 -0600

    feat: enhance AppException to support dynamic error messages with parameters

--- src/common/errors/app.exception.ts
+++ src/common/errors/app.exception.ts
@@ -3,14 +3,25 @@ import { ErrorCatalog, ErrorKey } from './error-catalog';
 import { ErrorResponse } from './error.types';
 
 export class AppException extends HttpException {
-  constructor(errorKey: ErrorKey) {
+  constructor(
+    errorKey: ErrorKey,
+    params?: Record<string, string>,
+  ) {
     const error = ErrorCatalog[errorKey];
+
+    let message: string = error.userMessage;
+
+    if (params) {
+      Object.entries(params).forEach(([key, value]) => {
+        message = message.replace(`{${key}}`, value);
+      });
+    }
 
     const response: ErrorResponse = {
       data: null,
       msg: {
         code: error.code,
-        msg: error.userMessage,
+        msg: message,
       },
     };
 
