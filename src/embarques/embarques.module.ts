6185973215368c4688da9b144512e497d5d6e199
camisplai <camjarag12gmail.com>
2026-08-13 15:25:21 -0600

    feat: enhance embarque functionality with new DTOs and import confirmation logic

--- src/embarques/embarques.module.ts
+++ src/embarques/embarques.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { EmbarquesService } from './embarques.service';
 import { EmbarquesController } from './embarques.controller';
 import { Embarque } from './entities/embarque.entity';
+import { Cliente } from 'src/clientes/entities/cliente.entity';
+import { Empleado } from 'src/empleados/entities/empleado.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Embarque])],
+  imports: [TypeOrmModule.forFeature([Embarque, Cliente, Empleado])],
   controllers: [EmbarquesController],
   providers: [EmbarquesService],
 })
